034dc70b131387cd5dff9675c294a5d849b49764
jdurrant <jacobdurrant@gmail.com>
2021-08-23 22:01:42 -0400

    Cleaned up index.html a bit.

--- html-javascript/dist/org.transcrypt.__runtime__.js
+++ html-javascript/dist/org.transcrypt.__runtime__.js
@@ -1,4 +1,4 @@
-// Transcrypt'ed from Python, 2021-08-22 00:46:40
+// Transcrypt'ed from Python, 2021-08-23 22:00:28
 var __name__ = 'org.transcrypt.__runtime__';
 export var __envir__ = {};
 __envir__.interpreter_name = 'python';
