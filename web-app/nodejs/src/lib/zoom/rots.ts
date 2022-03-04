a8fcab083276c14bd291515e5a423e2e58a75469
jdurrant <durrantj@pitt.edu>
2022-03-04 15:26:47 -0500

    Minor bug fix to zoom animation.

--- web-app/nodejs/src/lib/zoom/rots.ts
+++ web-app/nodejs/src/lib/zoom/rots.ts
@@ -9,7 +9,7 @@ export function zoomGetRotationAngles(params: any, mol: PDBMol): any[] {
     let minDist = params.zoom_min_dist;
     let maxDist = params.zoom_max_dist;
 
-    let deltaDist = (maxDist - minDist) / (params.frames - 1);
+    let deltaDist = (maxDist - minDist) / params.frames;
     for (let dist = maxDist; dist >= minDist + 0.1 * deltaDist; dist = dist - deltaDist) {
         rots.push([params.x_rot, params.y_rot, params.z_rot, dist]);
     }
